e877ab5eb98cb59915a5fc7efb9348efacc61e33
shivomv <shivom.iert@gmail.com>
2025-02-05 23:30:34 +0530

    location dropdown and nav dropdown

--- frontend/src/Components/CountryDropdown/CountryDropdown.jsx
+++ frontend/src/Components/CountryDropdown/CountryDropdown.jsx
@@ -1,18 +1,87 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { Button, TextField } from '@mui/material'
+import React, { useState, useEffect } from 'react'
 import { FaAngleDown } from 'react-icons/fa'
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import axios from 'axios';
 
 const CountryDropdown = () => {
+    const [open, setOpen] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [locations, setLocations] = useState([]);
+    const [selectedLocation, setSelectedLocation] = useState('');
+
+    useEffect(() => {
+        const fetchLocations = async () => {
+            try {
+                const response = await axios.get('https://countriesnow.space/api/v0.1/countries');
+                console.log(response.data.data);
+                const indiaCities = response.data.data.find(country => country.iso2 === "IN").cities;
+                setLocations(indiaCities);
+            } catch (error) {
+                console.error("Error fetching locations:", error);
+            }
+        };
+
+        fetchLocations();
+    }, []);
+    
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setSearchTerm(''); // Clear the search term on close
+    };
+
+    const handleLocationSelect = (location) => {
+        setSelectedLocation(location);
+        handleClose();
+    };
+
+    const filteredLocations = locations.filter(location =>
+        location.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <Button className='countryDropdown'>
-            <div className="info d-flex flex-column">
-                <span className='label'>Your Location</span>
-                <span className='name'>India</span>
-            </div>
-            {/* <span className='ml-auto'>
-                <FaAngleDown style={{ marginLeft: 'auto' }} />
-            </span> */}
-        </Button>
+        <>
+            <Button className='countryDropdown' onClick={handleClickOpen}>
+                <div className="info d-flex flex-column">
+                    <span className='label'>Your Location</span>
+                    <span className='name'>{selectedLocation || 'India'}</span>
+                </div>
+                <FaAngleDown />
+            </Button>
+            <Dialog open={open} onClose={handleClose} className='locationModal' PaperProps={{ style: { borderRadius: '15px' } }}>
+              <DialogTitle style={{ fontWeight: 'bold' }}>CHOOSE YOUR DELIVERY LOCATION</DialogTitle>
+              <DialogContent className='locationModalContent'>
+                <div className='searchContainer'>
+                <TextField 
+                  variant="outlined" 
+                  placeholder="Search for locations" 
+                  fullWidth 
+                  style={{ marginBottom: '20px', borderRadius: '5px', height: '50px'  }} // Increased height for search
+                  InputProps={{ style: { height: '50px' } }} // Ensuring the input field is larger
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                </div>
+                <ul className='locationList'>
+                  {filteredLocations.map((location, index) => (
+                    <li className='locationItem' key={index} onClick={() => handleLocationSelect(location)}>
+                      {location}
+                    </li>
+                  ))}
+                </ul>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} variant='contained' color="primary">Close</Button>
+              </DialogActions>
+            </Dialog>
+        </>
     )
 }
 
--- frontend/src/Components/Header/Navigation/Navigation.jsx
+++ frontend/src/Components/Header/Navigation/Navigation.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
         <nav >
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-sm-3 navPart1 align-items-center">
+                    <div className="col-sm-2 navPart1 align-items-center">
                         <Button className='allCatTab'>
                             <span className='icon1 me-2'><IoIosMenu /></span>
                             <span className='allCatTabText text text-uppercase'>All Categories</span>
@@ -16,14 +16,32 @@ const Navigation = () => {
                         </Button>
                     </div>
 
-                    <div className="col-sm-9 navPart2 d-flex align-items-center justify-content-center">
-                        <ul className='list list-inline d-flex ml-auto'> 
-                            <li className='inline-item me-3'><Link to='/' className='text-decoration-none'>Home</Link></li>
-                            <li className='inline-item me-3'><Link to='/fashion' >Fashion</Link></li>
-                            <li className='inline-item me-3'><Link to='/electronics' >Electronics</Link></li>
+                    <div className="col-sm-10 navPart2 d-flex align-items-center justify-content-center">
+                        <ul className='list list-inline d-flex ml-auto'>
+                            <li className='inline-item me-3'><Link to='/' className='text-decoration-none'>Home</Link>
+                                <div className='subMenu'>
+                                    <Link to='/home/overview'>Overview</Link>
+                                    <Link to='/home/latest'>Latest Updates</Link>
+                                </div>
+                            </li>
+                            <li className='inline-item me-3'><Link to='/fashion' >Fashion</Link>
+                                <div className='subMenu'>
+                                    <Link to='/fashion/men'>Men</Link>
+                                    <Link to='/fashion/women'>Women</Link>
+                                    <Link to='/fashion/accessories'>Accessories</Link>
+                                </div>
+                            </li>
+                            <li className='inline-item me-3'><Link to='/electronics' >Electronics</Link>
+                                <div className='subMenu'>
+                                    <Link to='/electronics/phones'>Phones</Link>
+                                    <Link to='/electronics/laptops'>Laptops</Link>
+                                    <Link to='/electronics/accessories'>Accessories</Link>
+                                </div>
+                            </li>
                             <li className='inline-item me-3'><Link to='/mobile' >Mobile</Link></li>
                             <li className='inline-item me-3'><Link to='/grocery' >Grocery</Link></li>
                             <li className='inline-item me-3'><Link to='/blog' >Blog</Link></li>
+                            <li className='inline-item me-3'><Link to='/about' >About</Link></li>
                             <li className='inline-item'><Link to='/contact' >Contact</Link></li>
                         </ul>
                     </div>
